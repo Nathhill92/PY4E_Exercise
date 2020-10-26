72e42741cb2093b5147d49d62a29779d11b7d163
Nathhill92 <Nathan.Hill148@gmail.com>
2020-10-25 23:28:58 -0500

    Create MajorityVote.py

---
+++ Code Challenges/MajorityVote.py
@@ -0,0 +1,27 @@
+# Majority Vote
+# Create a function that returns the majority vote in a list. A majority vote is an element that occurs > N/2 times in a list (where N is the length of the list).
+# Examples
+# majority_vote(["A", "A", "B"]) ➞ "A"
+# majority_vote(["A", "A", "A", "B", "C", "A"]) ➞ "A"
+# majority_vote(["A", "B", "B", "A", "C", "C"]) ➞ None
+# Notes
+# The frequency of the majority element must be strictly greater than 1/2.
+# If there is no majority element, return None.
+# If the list is empty, return None.
+from collections import Counter
+
+def majority_vote(votes):
+    # Create Count dictionary with counts of unique values
+    vote_count = Counter(votes)
+    # Return highest value or None 
+    for entry in vote_count:
+        if vote_count[entry] > len(votes)/2:
+            print(votes,"->",entry)
+            return entry
+    print(votes," -> None")
+    return None
+    
+#Challenge Examples
+majority_vote(["A", "A", "B"])
+majority_vote(["A", "A", "A", "B", "C", "A"])
+majority_vote(["A", "B", "B", "A", "C", "C"])
